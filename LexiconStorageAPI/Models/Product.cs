ab18f53fbffa843aa2c7c4e69d6050a4e6d0406c
Johanna Sewring <j_sewring@hotmail.com>
2026-06-08 13:41:14 +0200

    add storage model

---
+++ LexiconStorageAPI/Models/Product.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LexiconStorageAPI.Models
+{
+    public class Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; } = string.Empty;
+
+        [Range(0, int.MaxValue)]
+        public int Price { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Category { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(10)]
+        public string Shelf { get; set; } = string.Empty;
+
+        [Range(0, int.MaxValue)]
+        public int Count { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Description { get; set; } = string.Empty;
+    }
+}
